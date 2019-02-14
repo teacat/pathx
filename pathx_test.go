321a98c4a6b713f8ffad3a94428a78f4a53bd594
YamiOdymel <yamiodymel@gmail.com>
2019-02-14 09:47:13 +0800

    Fixed the test with local slash

--- pathx_test.go
+++ pathx_test.go
@@ -1,6 +1,7 @@
 package pathx
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -10,17 +11,17 @@ import (
 func TestPath(t *testing.T) {
 	assert := assert.New(t)
 	path := "a/b/c/d"
-	assert.Equal(`a\b\c\d`, Path(path))
+	assert.Equal(fmt.Sprintf("a%sb%sc%sd", separator, separator, separator), Path(path))
 	path = "a/b/c/d/"
-	assert.Equal(`a\b\c\d`, Path(path))
+	assert.Equal(fmt.Sprintf("a%sb%sc%sd%s", separator, separator, separator, separator), Path(path))
 }
 
 func TestPathDir(t *testing.T) {
 	assert := assert.New(t)
 	path := "a/b/c/d"
-	assert.Equal(`a\b\c\d\`, PathDir(path))
+	assert.Equal(fmt.Sprintf("a%sb%sc%sd%s", separator, separator, separator, separator), PathDir(path))
 	path = "a/b/c/d/"
-	assert.Equal(`a\b\c\d\`, PathDir(path))
+	assert.Equal(fmt.Sprintf("a%sb%sc%sd%s", separator, separator, separator, separator), PathDir(path))
 }
 
 func TestJoin(t *testing.T) {
@@ -28,9 +29,9 @@ func TestJoin(t *testing.T) {
 	path := "a/b/c/"
 	path2 := "/d/e/f/"
 	path3 := "g/h/i/"
-	assert.Equal(`a\b\c\d\e\f\g\h\i`, Join(path, path2, path3))
+	assert.Equal(fmt.Sprintf("a%sb%sc%sd%se%sf%sg%sh%si", separator, separator, separator, separator, separator, separator, separator, separator), Join(path, path2, path3))
 	path3 = "g/h/i"
-	assert.Equal(`a\b\c\d\e\f\g\h\i`, Join(path, path2, path3))
+	assert.Equal(fmt.Sprintf("a%sb%sc%sd%se%sf%sg%sh%si", separator, separator, separator, separator, separator, separator, separator, separator), Join(path, path2, path3))
 }
 
 func TestJoinDir(t *testing.T) {
@@ -38,9 +39,9 @@ func TestJoinDir(t *testing.T) {
 	path := "a/b/c/"
 	path2 := "/d/e/f/"
 	path3 := "g/h/i"
-	assert.Equal(`a\b\c\d\e\f\g\h\i\`, JoinDir(path, path2, path3))
+	assert.Equal(fmt.Sprintf("a%sb%sc%sd%se%sf%sg%sh%si%s", separator, separator, separator, separator, separator, separator, separator, separator, separator), JoinDir(path, path2, path3))
 	path3 = "g/h/i/"
-	assert.Equal(`a\b\c\d\e\f\g\h\i\`, JoinDir(path, path2, path3))
+	assert.Equal(fmt.Sprintf("a%sb%sc%sd%se%sf%sg%sh%si%s", separator, separator, separator, separator, separator, separator, separator, separator, separator), JoinDir(path, path2, path3))
 }
 func TestName(t *testing.T) {
 	assert := assert.New(t)
@@ -62,7 +63,7 @@ func TestExt(t *testing.T) {
 func TestContains(t *testing.T) {
 	assert := assert.New(t)
 	path := "a/b/c/d/e"
-	assert.True(Contains(path, `c\d\e`))
+	assert.True(Contains(path, fmt.Sprintf("c%sd%se", separator, separator)))
 }
 
 func TestExecutable(t *testing.T) {
